26dd3228daf0952001fb067286572df1fb24e0f2
MarkPartola <partolamark@yandex.ru>
2017-03-07 17:16:21 +0300

    Простейший прототип сервера.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 // @flow
+
+import { Server } from 'net';
+
+const server = new Server();
+
+server.on('connection', (client) => {
+
+  client.on('data', (data) => {
+    console.log(data.toString());
+    client.write('HTTP/1.1 200 OK');
+    client.end();
+  });
+});
+
+server.on('listening', () => {
+  console.log('Listening on 127.0.0.1:8080');
+});
+
+server.on('close', () => {
+  console.log('Server stopped.');
+});
+
+server.on('error', (error) => {
+  console.log('An error occurred.');
+  console.log(error);
+});
+
+server.listen(8080);
