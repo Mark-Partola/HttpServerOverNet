5332fda4a5f3f2d43cded8ff0656c0829789da86
MarkPartola <partolamark@yandex.ru>
2017-03-07 20:49:55 +0300

    Отделил заголовки запроса

--- src/Connection.js
+++ src/Connection.js
@@ -3,13 +3,21 @@
 export default class Connection {
 
   constructor(client) {
+    this.buffer = new Buffer([]);
+
     client.on('data', (data) => {
-      global.console.log(data);
+      this.buffer = Buffer.concat([this.buffer, data]);
+
+      const headersDelimiter = new Buffer('\r\n\r\n');
+      const indexOfDelimiter = data.indexOf(headersDelimiter);
+
+      if (indexOfDelimiter !== -1) {
+        const headers = this.buffer.slice(0, indexOfDelimiter).toString();
+        console.log(headers);
+      }
     });
 
-    client.on('end', () => {
-
-    });
+    client.on('end', () => console.log(this.buffer.toString()));
 
     client.on('close', (data) => {
       global.console.log('close');
